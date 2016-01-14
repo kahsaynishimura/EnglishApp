22537c92a63460afade8a5971bcc198df137cac0
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2016-01-14 14:58:39 -0200

    only the partner owner of the product is able to validate it

--- cakephp/app/Controller/TradesController.php
+++ cakephp/app/Controller/TradesController.php
@@ -32,7 +32,7 @@ class TradesController extends AppController {
             $this->set(array(
                 'trades' => $this->Trade->find('all', array(
                     'conditions' => array('user_id' => 3), // $this->request->data['Trade']['user_id']),
-                    'fields' => array('id','qr_code', 'validated'),
+                    'fields' => array('id', 'qr_code', 'validated'),
                     'contain' => array('Product' => array('fields' => array(
                                 'name'
                             ), 'Partner' => array('fields' => array('name', 'address', 'phone')))),
@@ -74,17 +74,30 @@ class TradesController extends AppController {
     }
 
     public function validateQR() {
-        $this->Trade->recursive = 0;
         if ($this->request->is(array('post', 'xml')) &&
                 !empty($this->request->data['Trade']['qr_code'])) {
+            $this->Trade->Behaviors->load('Containable');
             $trade = $this->Trade->find('first', array(
-                'fields' => array('id', 'Product.id', 'validated'),
+                'fields' => array('id', 'validated'),
+                'contain' => array(
+                    'Product' => array(
+                        'fields' => array('id', 'partner_id'),
+                        'Partner' => array('fields' => array('id', 'user_id'))
+                    )),
                 'conditions' => array('qr_code' => $this->request->data['Trade']['qr_code'])));
             $this->Trade->id = $trade['Trade']['id'];
-            if (sizeof($trade) == 2 && (int) $trade['Trade']['validated'] !== 1 && $this->Trade->saveField('validated', 1)) {//sizeof($trade) == 2 means that there is a trade object and a product object
-                $general_response = array('status' => 'success', 'data' => $trade['Product']['id'], 'message' => __('The code was validated'));
+            
+            //if the user trying to validate is the partner 
+            //who registered the product and the trade was not
+            // validated yet and there is a trade with this qr_code 
+            // in the database and if the field was saved
+            //TODO checar se o qr code construido da mesma forma retorna o mesmo resultado.
+            if (sizeof($trade) == 2 && (int) $trade['Product']['Partner']['user_id'] == (int) $this->request->data['Partner']['user_id'] &&
+                    (int) $trade['Trade']['validated'] !== 1 && $this->Trade->saveField('validated', 1)) {
+                //sizeof($trade) == 2 means that there is a trade object and a product object
+                $general_response = array('status' => 'success', 'data' => $trade['Product']['id'], 'message' => __('The code was validated.'));
             } else {
-                $general_response = array('status' => 'failure', 'data' => '', 'message' => __('This item was not found or is already validated.'));
+                $general_response = array('status' => 'failure', 'data' => '', 'message' => __('There was an error during the validation process.'));
             }
             $this->set(array(
                 'general_response' => $general_response,
