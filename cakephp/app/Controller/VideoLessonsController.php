e81ba81a09cff044fc357f3a9b76178e406bcd75
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2016-05-11 21:42:51 -0300

    video lessons

--- cakephp/app/Controller/VideoLessonsController.php
+++ cakephp/app/Controller/VideoLessonsController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * VideoLessons Controller
  *
@@ -8,12 +10,11 @@ App::uses('AppController', 'Controller');
  */
 class VideoLessonsController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	
+    /**
+     * Components
+     *
+     * @var array
+     */
     public $components = array('Paginator', 'RequestHandler');
 
     public function beforeFilter() {
@@ -24,111 +25,112 @@ class VideoLessonsController extends AppController {
 
     public function index_api() {
         $this->VideoLesson->recursive = 0;
-          $this->VideoLesson->Behaviors->load('Containable');
+        $this->VideoLesson->Behaviors->load('Containable');
         if ($this->request->is('xml')) {
             $videoLesson = $this->VideoLesson->find('all', array(
-                'fields' =>array('VideoLesson.id','id_video','name'),
-                'contain'=>array( 
-                    'VideoLessonScript'=>array(
-                        'VideoLessonScript.id', 
-                        'text_to_show', 
+                'fields' => array('VideoLesson.id', 'id_video', 'name'),
+                'contain' => array(
+                    'VideoLessonScript' => array(   
+                        'id',
+                        'text_to_show',
                         'text_to_check',
                         'video_lesson_id',
                         'stop_at_seconds')
-                    ),
-                'conditions' => array('VideoLesson.id' => 1),
+                ),
+                'conditions' => array('VideoLesson.id' => $this->request->data['VideoLesson']['id']),
                 'order' => array('VideoLesson.id ASC'),));
-            
+
             $this->set(array(
-            'VideoLesson' =>$videoLesson,
-            '_serialize' => 'VideoLesson'));
+                'VideoLesson' => $videoLesson,
+                '_serialize' => 'VideoLesson'));
         }
     }
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->VideoLesson->recursive = 0;
-		$this->set('videoLessons', $this->Paginator->paginate());
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->VideoLesson->recursive = 0;
+        $this->set('videoLessons', $this->Paginator->paginate());
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->VideoLesson->exists($id)) {
-			throw new NotFoundException(__('Invalid video lesson'));
-		}
-		$options = array('conditions' => array('VideoLesson.' . $this->VideoLesson->primaryKey => $id));
-		$this->set('videoLesson', $this->VideoLesson->find('first', $options));
-	}
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        if (!$this->VideoLesson->exists($id)) {
+            throw new NotFoundException(__('Invalid video lesson'));
+        }
+        $options = array('conditions' => array('VideoLesson.' . $this->VideoLesson->primaryKey => $id));
+        $this->set('videoLesson', $this->VideoLesson->find('first', $options));
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->VideoLesson->create();
-			if ($this->VideoLesson->save($this->request->data)) {
-				$this->Flash->success(__('The video lesson has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The video lesson could not be saved. Please, try again.'));
-			}
-		}
-	}
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->VideoLesson->create();
+            if ($this->VideoLesson->save($this->request->data)) {
+                $this->Flash->success(__('The video lesson has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The video lesson could not be saved. Please, try again.'));
+            }
+        }
+    }
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->VideoLesson->exists($id)) {
-			throw new NotFoundException(__('Invalid video lesson'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->VideoLesson->save($this->request->data)) {
-				$this->Flash->success(__('The video lesson has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The video lesson could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('VideoLesson.' . $this->VideoLesson->primaryKey => $id));
-			$this->request->data = $this->VideoLesson->find('first', $options);
-		}
-	}
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        if (!$this->VideoLesson->exists($id)) {
+            throw new NotFoundException(__('Invalid video lesson'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->VideoLesson->save($this->request->data)) {
+                $this->Flash->success(__('The video lesson has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The video lesson could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('VideoLesson.' . $this->VideoLesson->primaryKey => $id));
+            $this->request->data = $this->VideoLesson->find('first', $options);
+        }
+    }
 
-/**
- * delete method
- * 
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->VideoLesson->id = $id;
-		if (!$this->VideoLesson->exists()) {
-			throw new NotFoundException(__('Invalid video lesson'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->VideoLesson->delete()) {
-			$this->Flash->success(__('The video lesson has been deleted.'));
-		} else {
-			$this->Flash->error(__('The video lesson could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+    /**
+     * delete method
+     * 
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        $this->VideoLesson->id = $id;
+        if (!$this->VideoLesson->exists()) {
+            throw new NotFoundException(__('Invalid video lesson'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->VideoLesson->delete()) {
+            $this->Flash->success(__('The video lesson has been deleted.'));
+        } else {
+            $this->Flash->error(__('The video lesson could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
 }
