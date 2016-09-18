1c8aef7256f98198fb7a9392299b78936262fb88
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2016-09-18 20:37:31 -0300

    changes video script add and edit screens to use minutes and numbers instead of only secs

--- cakephp/app/Controller/VideoLessonScriptsController.php
+++ cakephp/app/Controller/VideoLessonScriptsController.php
@@ -47,10 +47,36 @@ class VideoLessonScriptsController extends AppController {
      *
      * @return void
      */
+//    public function add($lesson_id) {
+//        if ($this->request->is('post')) {
+//            $this->VideoLessonScript->create();
+//            $this->request->data['VideoLessonScript']['video_lesson_id'] = $lesson_id;
+//            if ($this->VideoLessonScript->save($this->request->data)) {
+//                $this->Flash->success(__('The video lesson script has been saved.'));
+//                return $this->redirect(array('action' => 'index', $this->request->data['VideoLessonScript']['video_lesson_id']));
+//            } else {
+//                $this->Flash->error(__('The video lesson script could not be saved. Please, try again.'));
+//            }
+//        }
+//        $options = array('conditions' => array('VideoLesson.id' => $lesson_id));
+//        $videoLesson = $this->VideoLessonScript->VideoLesson->find('first', $options);
+//        $this->set(compact('videoLesson'));
+//    }
+    /**
+     * add method
+     *
+     * @return void
+     */
     public function add($lesson_id) {
         if ($this->request->is('post')) {
             $this->VideoLessonScript->create();
             $this->request->data['VideoLessonScript']['video_lesson_id'] = $lesson_id;
+            $startSec = (int) $this->request->data['VideoLessonScript']['start_seconds'];
+            $startMin = (int) $this->request->data['VideoLessonScript']['start_minutes'];
+            $stopSec = (int) $this->request->data['VideoLessonScript']['stop_seconds'];
+            $stopMin = (int) $this->request->data['VideoLessonScript']['stop_minutes'];
+            $this->request->data['VideoLessonScript']['start_at_seconds'] = $startSec + $startMin * 60;
+            $this->request->data['VideoLessonScript']['stop_at_seconds'] = $stopSec + $stopMin * 60;
             if ($this->VideoLessonScript->save($this->request->data)) {
                 $this->Flash->success(__('The video lesson script has been saved.'));
                 return $this->redirect(array('action' => 'index', $this->request->data['VideoLessonScript']['video_lesson_id']));
@@ -75,6 +101,12 @@ class VideoLessonScriptsController extends AppController {
             throw new NotFoundException(__('Invalid video lesson script'));
         }
         if ($this->request->is(array('post', 'put'))) {
+            $startSec = (int) $this->request->data['VideoLessonScript']['start_seconds'];
+            $startMin = (int) $this->request->data['VideoLessonScript']['start_minutes'];
+            $stopSec = (int) $this->request->data['VideoLessonScript']['stop_seconds'];
+            $stopMin = (int) $this->request->data['VideoLessonScript']['stop_minutes'];
+            $this->request->data['VideoLessonScript']['start_at_seconds'] = $startSec + $startMin * 60;
+            $this->request->data['VideoLessonScript']['stop_at_seconds'] = $stopSec + $stopMin * 60;
             if ($this->VideoLessonScript->save($this->request->data)) {
                 $this->Flash->success(__('The video lesson script has been saved.'));
 
--- cakephp/app/View/VideoLessonScripts/edit.ctp
+++ cakephp/app/View/VideoLessonScripts/edit.ctp
@@ -7,9 +7,11 @@
         echo $this->Form->input('video_lesson_id');
         echo $this->Form->input('text_to_show');
         echo $this->Form->input('text_to_check');
-        echo $this->Form->input('stop_at_seconds');
         echo __('Stop should be greater than start');
-        echo $this->Form->input('start_at_seconds');
+        echo $this->Form->input('start_minutes', array('label' => 'Start Minute', 'type' => 'number', 'min' => '0'));
+        echo $this->Form->input('start_seconds', array('label' => 'Start Second', 'type' => 'number', 'min' => '0', 'max' => '60'));
+        echo $this->Form->input('stop_minutes', array('label' => 'Stop Minute', 'type' => 'number', 'min' => '0'));
+        echo $this->Form->input('stop_seconds', array('label' => 'Stop Second', 'type' => 'number', 'min' => '0', 'max' => '60'));
         ?>
     </fieldset>
     <?php echo $this->Form->end(__('Submit')); ?>
