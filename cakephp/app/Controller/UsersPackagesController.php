2550be7400867763e275bbda2ba96ef521b55d8a
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2016-12-20 20:36:58 -0200

    paypal integration
    with shop.echopractice.com

--- cakephp/app/Controller/UsersPackagesController.php
+++ cakephp/app/Controller/UsersPackagesController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * UsersPackages Controller
  *
@@ -8,103 +10,150 @@ App::uses('AppController', 'Controller');
  */
 class UsersPackagesController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator', 'RequestHandler');
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->UsersPackage->recursive = 0;
-		$this->set('usersPackages', $this->Paginator->paginate());
-	}
+    public function beforeFilter() {
+        parent::beforeFilter();
+        // Allow users to register and logout.
+        $this->Auth->allow('addPayPal');
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->UsersPackage->exists($id)) {
-			throw new NotFoundException(__('Invalid users package'));
-		}
-		$options = array('conditions' => array('UsersPackage.' . $this->UsersPackage->primaryKey => $id));
-		$this->set('usersPackage', $this->UsersPackage->find('first', $options));
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->UsersPackage->recursive = 0;
+        $this->set('usersPackages', $this->Paginator->paginate());
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->UsersPackage->create();
-			if ($this->UsersPackage->save($this->request->data)) {
-				$this->Flash->success(__('The users package has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The users package could not be saved. Please, try again.'));
-			}
-		}
-		$users = $this->UsersPackage->User->find('list');
-		$packages = $this->UsersPackage->Package->find('list');
-		$this->set(compact('users', 'packages'));
-	}
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        if (!$this->UsersPackage->exists($id)) {
+            throw new NotFoundException(__('Invalid users package'));
+        }
+        $options = array('conditions' => array('UsersPackage.' . $this->UsersPackage->primaryKey => $id));
+        $this->set('usersPackage', $this->UsersPackage->find('first', $options));
+    }
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->UsersPackage->exists($id)) {
-			throw new NotFoundException(__('Invalid users package'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->UsersPackage->save($this->request->data)) {
-				$this->Flash->success(__('The users package has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The users package could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('UsersPackage.' . $this->UsersPackage->primaryKey => $id));
-			$this->request->data = $this->UsersPackage->find('first', $options);
-		}
-		$users = $this->UsersPackage->User->find('list');
-		$packages = $this->UsersPackage->Package->find('list');
-		$this->set(compact('users', 'packages'));
-	}
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->UsersPackage->create();
+            if ($this->UsersPackage->save($this->request->data)) {
+                $this->Flash->success(__('The users package has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The users package could not be saved. Please, try again.'));
+            }
+        }
+        $users = $this->UsersPackage->User->find('list');
+        $packages = $this->UsersPackage->Package->find('list');
+        $this->set(compact('users', 'packages'));
+    }
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->UsersPackage->id = $id;
-		if (!$this->UsersPackage->exists()) {
-			throw new NotFoundException(__('Invalid users package'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->UsersPackage->delete()) {
-			$this->Flash->success(__('The users package has been deleted.'));
-		} else {
-			$this->Flash->error(__('The users package could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+    /**
+     * addPaypal method
+     *
+     * @return void
+     */
+    public function addPayPal() {
+        if ($this->request->is('post')) {
+            $this->UsersPackage->User->recursive = 0;
+            $buyerId = $this->UsersPackage->User->find('first', array(
+                'conditions' => array('username' => $this->request->data['payer_email']),
+                'fields' => array('User.id', 'User.username'))
+            );
+            $package = $this->UsersPackage->Package->find('first', array(
+                'conditions' => array('id' => $this->request->data['item_number1']),
+                'fields' => array('Package.id', 'Package.price')
+            ));
+            $newLicense = array('UsersPackage');
+            if (!empty($buyerId) && $this->request->data['receiver_email'] == 'karina@echopractice.com' &&
+                    ($this->request->data['payment_status'] == 'Completed') &&
+                    $this->request->data['mc_gross'] == $package['Package']['price']) {
+                $newLicense['UsersPackage']['transactionId'] = $this->request->data['txn_id'];
+                $newLicense['UsersPackage']['package_id'] = $this->request->data['item_number1'];
+                $newLicense['UsersPackage']['user_id'] = $buyerId['User']['id'];
+            }
+            $this->log('buyerId: ' . $buyerId['User']['id'] . '|' . 'PackageId: ' . $this->request->data['item_number1']);
+
+            $this->log($this->request->data);
+            $this->UsersPackage->create();
+            if ($this->UsersPackage->save($newLicense)) {
+//                return $this->redirect(array('action' => 'index'));
+                $this->log('saved');
+            } else {
+                $this->log('there was a problem recording the purchase of this package. Check: email buyer and receiver, payment status and item number');
+            }
+        }
+        $users = $this->UsersPackage->User->find('list');
+        $packages = $this->UsersPackage->Package->find('list');
+        $this->set(compact('users', 'packages'));
+    }
+
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        if (!$this->UsersPackage->exists($id)) {
+            throw new NotFoundException(__('Invalid users package'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->UsersPackage->save($this->request->data)) {
+                $this->Flash->success(__('The users package has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The users package could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('UsersPackage.' . $this->UsersPackage->primaryKey => $id));
+            $this->request->data = $this->UsersPackage->find('first', $options);
+        }
+        $users = $this->UsersPackage->User->find('list');
+        $packages = $this->UsersPackage->Package->find('list');
+        $this->set(compact('users', 'packages'));
+    }
+
+    /**
+     * delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        $this->UsersPackage->id = $id;
+        if (!$this->UsersPackage->exists()) {
+            throw new NotFoundException(__('Invalid users package'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->UsersPackage->delete()) {
+            $this->Flash->success(__('The users package has been deleted.'));
+        } else {
+            $this->Flash->error(__('The users package could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
 }
---
+++ cakephp/app/View/UsersPackages/add_pay_pal.ctp
@@ -0,0 +1,22 @@
+<div class="usersPackages form">
+<?php echo $this->Form->create('UsersPackage'); ?>
+	<fieldset>
+		<legend><?php echo __('Add Users Package'); ?></legend>
+	<?php
+		echo $this->Form->input('user_id');
+		echo $this->Form->input('package_id');
+	?>
+	</fieldset>
+<?php echo $this->Form->end(__('Submit')); ?>
+</div>
+<div class="actions">
+	<h3><?php echo __('Actions'); ?></h3>
+	<ul>
+
+		<li><?php echo $this->Html->link(__('List Users Packages'), array('action' => 'index')); ?></li>
+		<li><?php echo $this->Html->link(__('List Users'), array('controller' => 'users', 'action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New User'), array('controller' => 'users', 'action' => 'add')); ?> </li>
+		<li><?php echo $this->Html->link(__('List Packages'), array('controller' => 'packages', 'action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New Package'), array('controller' => 'packages', 'action' => 'add')); ?> </li>
+	</ul>
+</div>
